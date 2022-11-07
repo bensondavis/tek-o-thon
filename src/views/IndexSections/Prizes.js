8dbc893fe7982dfa6b5e89527a34f7e1ac976f2c
Benson Davis <bensondavis61@gmail.com>
2022-11-07 18:21:07 +0530

    added some text

--- src/views/IndexSections/Prizes.js
+++ src/views/IndexSections/Prizes.js
@@ -3,16 +3,10 @@ import PrizesSingle from "./PrizesSingle";
 import { prizeData } from "./PrizeData";
 import { Container } from "reactstrap";
 import { Typography } from "@mui/joy";
-// import Association from "./Association"
 
 export default function Prizes() {
   return (
     <div className="section section-signup">
-      {/* <img alt="..." className="path" src={require("assets/img/path3.png")}  /> */}
-      {/* <div className="squares square-1" />
-      <div className="squares square-2" />
-      <div className="squares square-3" />
-      <div className="squares square-4" />  */}
       <Container>
       <Typography fontSize={"3rem"} fontWeight={550} sx={{textAlign:"center", color: "white"}}>Prizes</Typography>
       <Stack
@@ -32,7 +26,6 @@ export default function Prizes() {
         ))}
       </Stack>
       </Container>
-      {/* <Association/> */}
     </div>
   );
 }
